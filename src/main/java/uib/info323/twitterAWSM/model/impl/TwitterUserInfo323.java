0881ff318a587eadf6e109c6a821c2958b1ad1a6
Lisa <halvorsen.lisa@gmail.com>
2012-04-29 12:33:55 +0200

    User impl og interface

--- src/main/java/uib/info323/twitterAWSM/model/impl/TwitterUserInfo323.java
+++ src/main/java/uib/info323/twitterAWSM/model/impl/TwitterUserInfo323.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 
 import uib.info323.twitterAWSM.model.interfaces.ITwitterUserInfo323;
 
+/**
+ * @author Lisa
+ * 
+ */
 @Component
 public class TwitterUserInfo323 implements ITwitterUserInfo323 {
 
@@ -18,80 +22,172 @@ public class TwitterUserInfo323 implements ITwitterUserInfo323 {
 	private String description;
 	private String location;
 	private Date createdDate;
+	private int favoritesCount;
+	private int followersCount;
+	private int friendsCount;
+	private String language;
+	private String profileUrl;
+	private int statusesCount;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param fitnessScore
+	 * @param id
+	 * @param screenName
+	 * @param name
+	 * @param url
+	 * @param profileImageUrl
+	 * @param description
+	 * @param location
+	 * @param createdDate
+	 * @param favoritesCount
+	 * @param followersCount
+	 * @param friendsCount
+	 * @param language
+	 * @param profileUrl
+	 * @param statusesCount
+	 */
+	public TwitterUserInfo323(int fitnessScore, long id, String screenName,
+			String name, String url, String profileImageUrl,
+			String description, String location, Date createdDate,
+			int favoritesCount, int followersCount, int friendsCount,
+			String language, String profileUrl, int statusesCount) {
+		this.fitnessScore = fitnessScore;
+		this.id = id;
+		this.screenName = screenName;
+		this.name = name;
+		this.url = url;
+		this.profileImageUrl = profileImageUrl;
+		this.description = description;
+		this.location = location;
+		this.createdDate = createdDate;
+		this.favoritesCount = favoritesCount;
+		this.followersCount = followersCount;
+		this.friendsCount = friendsCount;
+		this.language = language;
+		this.profileUrl = profileUrl;
+		this.statusesCount = statusesCount;
+	}
 
 	public int getFitnessScore() {
-
 		return fitnessScore;
 	}
 
-	public Date getCreatedDate() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public int getFavoritesCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public int getFollowersCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public int getFriendsCount() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void setFitnessScore(int fitnessScore) {
+		this.fitnessScore = fitnessScore;
 	}
 
 	public long getId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return id;
 	}
 
-	public String getLanguage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getLocation() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getProfileImageUrl() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getProfileUrl() {
-		// TODO Auto-generated method stub
-		return null;
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public String getScreenName() {
-		// TODO Auto-generated method stub
-		return null;
+		return screenName;
 	}
 
-	public int getStatusesCount() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void setScreenName(String screenName) {
+		this.screenName = screenName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getUrl() {
-		// TODO Auto-generated method stub
-		return null;
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getProfileImageUrl() {
+		return profileImageUrl;
+	}
+
+	public void setProfileImageUrl(String profileImageUrl) {
+		this.profileImageUrl = profileImageUrl;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public int getFavoritesCount() {
+		return favoritesCount;
+	}
+
+	public void setFavoritesCount(int favoritesCount) {
+		this.favoritesCount = favoritesCount;
+	}
+
+	public int getFollowersCount() {
+		return followersCount;
+	}
+
+	public void setFollowersCount(int followersCount) {
+		this.followersCount = followersCount;
+	}
+
+	public int getFriendsCount() {
+		return friendsCount;
+	}
+
+	public void setFriendsCount(int friendsCount) {
+		this.friendsCount = friendsCount;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getProfileUrl() {
+		return profileUrl;
+	}
+
+	public void setProfileUrl(String profileUrl) {
+		this.profileUrl = profileUrl;
+	}
+
+	public int getStatusesCount() {
+		return statusesCount;
+	}
+
+	public void setStatusesCount(int statusesCount) {
+		this.statusesCount = statusesCount;
 	}
 
 }
--- src/main/java/uib/info323/twitterAWSM/model/interfaces/ITwitterUserInfo323.java
+++ src/main/java/uib/info323/twitterAWSM/model/interfaces/ITwitterUserInfo323.java
@@ -6,33 +6,63 @@ public interface ITwitterUserInfo323 {
 
 	public int getFitnessScore();
 
+	public void setFitnessScore(int fitnessScore);
+
 	// methods from TwitterProfile
 	public Date getCreatedDate();
 
+	public void setCreatedDate(Date createdDate);
+
 	public String getDescription();
+
+	public void setDescription(String description);
 
 	public int getFavoritesCount();
 
+	public void setFavoritesCount(int favoritesCount);
+
 	public int getFollowersCount();
+
+	public void setFollowersCount(int followersCount);
 
 	public int getFriendsCount();
 
+	public void setFriendsCount(int friendsCount);
+
 	public long getId();
+
+	public void setId(long id);
 
 	public String getLanguage();
 
+	public void setLanguage(String language);
+
 	public String getLocation();
+
+	public void setLocation(String location);
 
 	public String getName();
 
+	public void setName(String name);
+
 	public String getProfileImageUrl();
+
+	public void setProfileImageUrl(String profileImageUrl);
 
 	public String getProfileUrl();
 
+	public void setProfileUrl(String profileUrl);
+
 	public String getScreenName();
+
+	public void setScreenName(String screenName);
 
 	public int getStatusesCount();
 
+	public void setStatusesCount(int statusesCount);
+
 	public String getUrl();
+
+	public void setUrl(String url);
 
 }
