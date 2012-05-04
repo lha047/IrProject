76d30be8e14baf0a9de61a781ca82cbd0dffd6ec
Snorre Magnus Davøen <snorre.tjenester@gmail.com>
2012-05-04 11:24:58 +0200

    Added Gson to dependencies. Also added an io-package for object factories...

---
+++ src/main/java/uib/info323/twitterAWSM/io/AbstractFeedJamFactory.java
@@ -0,0 +1,10 @@
+package uib.info323.twitterAWSM.io;
+
+public abstract class AbstractFeedJamFactory {
+	
+	public AbstractFeedJamFactory() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ src/main/java/uib/info323/twitterAWSM/io/TweetFactoryIntf.java
@@ -0,0 +1,7 @@
+package uib.info323.twitterAWSM.io;
+
+public interface TweetFactoryIntf {
+	
+	public INFO323Tweet 
+
+}
