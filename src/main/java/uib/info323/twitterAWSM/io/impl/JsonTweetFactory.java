cc563e343bf803b380b6345bffd83d8279fe1981
Snorre Magnus Davøen <snorre.tjenester@gmail.com>
2012-05-04 13:25:58 +0200

    Comitting latest pom and jsontweetfactory

--- src/main/java/uib/info323/twitterAWSM/io/impl/JsonTweetFactory.java
+++ src/main/java/uib/info323/twitterAWSM/io/impl/JsonTweetFactory.java
@@ -3,6 +3,9 @@ package uib.info323.twitterAWSM.io.impl;
 import java.util.Date;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import uib.info323.twitterAWSM.io.TweetFactoryIntf;
 import uib.info323.twitterAWSM.model.impl.TweetInfo323;
 
@@ -17,6 +20,9 @@ public class JsonTweetFactory implements TweetFactoryIntf{
 	@Override
 	public List<TweetInfo323> searchTweets(String searchTerm) {
 		// TODO Auto-generated method stub
+		Gson response = new Gson();
+		GsonBuilder builder = new  GsonBuilder();
+		
 		return null;
 	}
 
