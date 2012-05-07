3e6b202e850608bd03232a79791e1792c63a5f53
Torstein Thune <torstein@gmail.com>
2012-05-07 16:43:10 +0200

    added logic for search, doesn't work, but same code works in HomeController

--- src/main/java/uib/info323/twitterAWSM/SearchController.java
+++ src/main/java/uib/info323/twitterAWSM/SearchController.java
@@ -7,10 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import uib.info323.twitterAWSM.io.AbstractFeedJamFactory;
+import uib.info323.twitterAWSM.io.TweetFactory;
+import uib.info323.twitterAWSM.io.impl.JsonFeedJamFactory;
 import uib.info323.twitterAWSM.model.interfaces.TweetInfo323;
+import uib.info323.twitterAWSM.model.interfaces.TweetSearchResults;
 
 /**
  * 
@@ -19,22 +24,24 @@ import uib.info323.twitterAWSM.model.interfaces.TweetInfo323;
  *
  */
 @Controller
-@RequestMapping(value = "/search/*")
+@RequestMapping(value = "/search", method = RequestMethod.GET)
 public class SearchController {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(SearchController.class);
 	
 	
-	@RequestMapping(value = "/tag")
-	public ModelAndView search(@RequestParam String q) {
+	public ModelAndView search(@RequestParam String q, int resultsPerPage) {
 		
 		ModelAndView mav = new ModelAndView("tagSearchResults");
 		
-		List<TweetInfo323> tweetList = new ArrayList<TweetInfo323>();
-		logger.info("Number of tweets matching " + "#"+q + " is " + tweetList.size());
+		JsonFeedJamFactory factory = (JsonFeedJamFactory) AbstractFeedJamFactory.getFactory(1);
+		TweetFactory tweetFactory = factory.getTweetFactory();
+		TweetSearchResults tweetList = tweetFactory.searchTweets(q, resultsPerPage);
+		//logger.info("Number of tweets matching " + "#"+q + " is " + tweetList.size());
 		logger.info("Searching for: " + "#"+q);
 		mav.addObject("query", q);
+		mav.addObject(tweetList);
 		mav.addObject("results", tweetList);
 		
 		return mav;
