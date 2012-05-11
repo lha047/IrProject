65ca315842283fb541fb8a052faba927a04c213a
Lisa Halvorsen <halvorsen.lisa@gmail.com>
2012-05-11 13:38:43 +0200

    addUser and search() works

--- src/main/java/uib/info323/twitterAWSM/io/impl/MySQLUserFactory.java
+++ src/main/java/uib/info323/twitterAWSM/io/impl/MySQLUserFactory.java
@@ -11,7 +11,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import uib.info323.twitterAWSM.io.UserDAO;
 import uib.info323.twitterAWSM.io.UserSearchFactory;
@@ -21,10 +23,12 @@ import uib.info323.twitterAWSM.model.interfaces.TwitterUserInfo323;
 
 public class MySQLUserFactory implements UserSearchFactory, UserDAO {
 
-	private static final String SQL_INSERT_USER = "insert into users(id, screen_name, name, url, profile_image_url, description, location, date, favorites_count, followers_count, friends_count, language, profile_url, statuses_count, fitness_score, last_updated) "
-			+ "values(:id, :screen_name, :name, :url, :profile_image_url, :description, :location, :date, :favorites_count, :followers_count, :friends_count, :language, :profile_url, :statuses_count, :fitness_score, :last_updated)";
+	private static final String SQL_INSERT_USER = "insert into users(ID, SCREEN_NAME, NAME, URL, PROFILE_IMAGE_URL, DESCRIPTION, LOCATION, CREATED_DATE, FAVORITES_COUNT, FOLLOWERS_COUNT, FRIENDS_COUNT, LANGUAGE, PROFILE_URL, STATUSES_COUNT, FITNESS_SCORE, LAST_UPDATED) "
+			+ "values(:ID, :SCREEN_NAME, :NAME, :URL, :PROFILE_IMAGE_URL, :DESCRIPTION, :LOCATION, :CREATED_DATE, :FAVORITES_COUNT, :FOLLOWERS_COUNT, :FRIENDS_COUNT, :LANGUAGE, :PROFILE_URL, :STATUSES_COUNT, :FITNESS_SCORE, :LAST_UPDATED)";
 
-	private static final String SQL_SELECT_USER_BY_SCREEN_NAME = null;
+	private static final String SQL_SELECT_USER_BY_SCREEN_NAME = "SELECT * FROM users WHERE SCREEN_NAME = :SCREEN_NAME";
+
+	private static final String SQL_SELECT_USER_BY_ID = "SELECT * FROM users WHERE ID = :ID";
 
 	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	private DateFormat dateFormat;
@@ -53,10 +57,12 @@ public class MySQLUserFactory implements UserSearchFactory, UserDAO {
 
 	@Override
 	public TwitterUserInfo323 searchUserByScreenName(String screenName) {
-		//
-		// return namedParameterJdbcTemplate.queryForObject(
-		// SQL_SELECT_USER_BY_SCREEN_NAME, ;
-		return null;
+		SqlParameterSource namedParameter = new MapSqlParameterSource(
+				"SCREEN_NAME", screenName);
+		TwitterUserInfo323Impl user = (TwitterUserInfo323Impl) namedParameterJdbcTemplate
+				.queryForObject(SQL_SELECT_USER_BY_SCREEN_NAME, namedParameter,
+						new UserRowMapper());
+		return user;
 	}
 
 	public static void main(String[] args) {
@@ -75,43 +81,61 @@ public class MySQLUserFactory implements UserSearchFactory, UserDAO {
 				23, 23, "No", "http://profile.url", 12);
 
 		System.out.println(userFactory.addUser(user));
-		userFactory.searchUserByNameId(1234);
+		System.out.println(userFactory.searchUserByNameId(2222).getId() + " "
+				+ userFactory.searchUserByNameId(2222).getScreenName());
+		System.out.println(userFactory.searchUserByNameId(2222).getId()
+				+ userFactory.searchUserByScreenName("screenName")
+						.getScreenName());
 	}
 
 	@Override
 	public TwitterUserInfo323 searchUserByNameId(long id) {
 
+		SqlParameterSource namedParameter = new MapSqlParameterSource("ID", id);
+		TwitterUserInfo323Impl user = (TwitterUserInfo323Impl) namedParameterJdbcTemplate
+				.queryForObject(SQL_SELECT_USER_BY_ID, namedParameter,
+						new UserRowMapper());
+		return user;
+	}
+
+	private Map<String, Object> mapSqlToUser() {
+		Map<String, Object> m = new HashMap<String, Object>();
+		// m.put(key, value)
 		return null;
 	}
 
 	@Override
 	public boolean addUser(TwitterUserInfo323 user) {
 		Map<String, Object> params = userToMap(user);
-		int inserted = namedParameterJdbcTemplate.update(SQL_INSERT_USER,
-				params);
-
+		int inserted = -1;
+		try {
+			inserted = namedParameterJdbcTemplate.update(SQL_INSERT_USER,
+					params);
+		} catch (Exception e) {
+			// TODO existing entety
+		}
 		return inserted == 1;
 	}
 
 	private Map<String, Object> userToMap(TwitterUserInfo323 user) {
 		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("id", user.getId());
-		params.put("screen_name", user.getScreenName());
-		params.put("name", user.getName());
-		params.put("url", user.getUrl());
-		params.put("profile_image_url", user.getProfileImageUrl());
-		params.put("description", user.getDescription());
-		params.put("location", user.getLocation());
-		params.put("date", user.getCreatedDate());
-		params.put("favorites_count", user.getFavoritesCount());
-		params.put("followers_count", user.getFollowersCount());
-		params.put("friends_count", user.getFriendsCount());
-		params.put("language", user.getLanguage());
-		params.put("profile_url", user.getProfileUrl());
-		params.put("statuses_count", user.getStatusesCount());
-		params.put("fitness_score", user.getFitnessScore());
+		params.put("ID", user.getId());
+		params.put("SCREEN_NAME", user.getScreenName());
+		params.put("NAME", user.getName());
+		params.put("URL", user.getUrl());
+		params.put("PROFILE_IMAGE_URL", user.getProfileImageUrl());
+		params.put("DESCRIPTION", user.getDescription());
+		params.put("LOCATION", user.getLocation());
+		params.put("CREATED_DATE", user.getCreatedDate());
+		params.put("FAVORITES_COUNT", user.getFavoritesCount());
+		params.put("FOLLOWERS_COUNT", user.getFollowersCount());
+		params.put("FRIENDS_COUNT", user.getFriendsCount());
+		params.put("LANGUAGE", user.getLanguage());
+		params.put("PROFILE_URL", user.getProfileUrl());
+		params.put("STATUSES_COUNT", user.getStatusesCount());
+		params.put("FITNESS_SCORE", user.getFitnessScore());
 		date = new Date();
-		params.put("last_updated", dateFormat.format(date));
+		params.put("LAST_UPDATED", dateFormat.format(date));
 		return params;
 	}
 
