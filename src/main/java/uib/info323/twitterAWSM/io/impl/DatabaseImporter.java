1a267bef9dda0ce6ed4ca514a347af38f49eb9a3
Snorre Magnus Davøen <snorre.tjenester@gmail.com>
2012-05-31 16:34:45 +0200

    Added threading to simulate concurrent requests in DbImporter

--- src/main/java/uib/info323/twitterAWSM/io/impl/DatabaseImporter.java
+++ src/main/java/uib/info323/twitterAWSM/io/impl/DatabaseImporter.java
@@ -9,32 +9,43 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.LinkedList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.core.task.TaskExecutor;
 
 import com.mysql.jdbc.Statement;
 
 public class DatabaseImporter {
-	
-	public static void main(String[] args) {
-		
+
+	public DatabaseImporter() {
+
+		start();
+
+	}
+
+	private void start() {
 		String url = "jdbc:mysql://localhost:3306/";
 		String driver = "com.mysql.jdbc.Driver";
 		String db = "feedjam";
 		String user = "root";
 		String pass = "info323";
 		Connection connection;
-		
+
 		try {
 			Class.forName(driver);
 			connection = DriverManager.getConnection(url+db, user, pass);
-			
+
 			BufferedReader reader = new BufferedReader(new FileReader(new File("feedjam.sql")));
-			
+
 			LinkedList<String> insertQueries = new LinkedList<String>();
 			StringBuilder insertString;
-			
+
 			insertString = new StringBuilder();
 			insertString.append(reader.readLine());
-			
+
 			String line;
 			while((line = reader.readLine()) != null) {
 				if(!line.startsWith("INSERT")) {
@@ -44,19 +55,13 @@ public class DatabaseImporter {
 					insertString = new StringBuilder();
 					insertString.append(line);
 				}
-				
-				
 			}
 			
-			for(String query : insertQueries) {
-				Statement stat = (Statement) connection.createStatement();
-				long start = System.currentTimeMillis();
-				stat.execute(query);
-				long duration = System.currentTimeMillis() -start;
-				System.out.println("Batch insert time: " + duration);
+			ExecutorService pool = Executors.newFixedThreadPool(40);
+			for(int i = 0; i < 40; i++) {
+				pool.execute(new DatabaseInserter(insertQueries, connection));
 			}
-			
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -70,9 +75,40 @@ public class DatabaseImporter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
 	}
+
+	private class DatabaseInserter implements Runnable {
+		
+		private LinkedList<String> insertQueries;
+		private Connection connection;
+		
+
+		public DatabaseInserter(LinkedList<String> insertQueries, Connection connection) {
+			this.insertQueries = new LinkedList<String>();
+			this.insertQueries.addAll(insertQueries);
+			this.connection = connection;
+		}
+
+		public void run() {
+			for(String query : insertQueries) {
+				Statement stat;
+				try {
+					stat = (Statement) connection.createStatement();
+					long start = System.currentTimeMillis();
+					stat.execute(query);
+					long duration = System.currentTimeMillis() -start;
+					System.out.println("Batch insert time: " + duration + " Thread: " + Thread.currentThread().getName());
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		new DatabaseImporter();
+	}
+
 
 }
